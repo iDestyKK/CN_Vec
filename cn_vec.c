d157d496ee98e814b34e8024d761cc09c83fc3b0
Clara Taylor <idesty@derpg.xyz>
2016-05-19 19:15:17 -0400

    Initial commit

---
+++ cn_vec.c
@@ -0,0 +1,191 @@
+/*
+ * CN_Vec Library
+ *
+ * Version 1.0.1 (Last Updated 2016-05-16)
+ *
+ * Description:
+ *     C lacks vectors from C++.
+ *     A CN_Vec is like a vector in C++, only you have to do some additional things for it to work.
+ *     If anything, this library will probably make you switch to C++ for your stuff. :P
+ *
+ *     Changelog of library is located at the bottom of "cn_vec.h"
+ *
+ * Author:
+ *     Clara Ann Nguyen
+ *
+ * For documentation and details on every function in this library, visit the following URL:
+ * http://web.eecs.utk.edu/~ssmit285/lib/cn_vec/index.html
+ */
+
+#include "cn_vec.h"
+
+//Functions
+//Initializer
+CN_VEC new_cn_vec(cnv_uint size) {
+    //Initializes the new vector. It needs the size of the data type we are storing in it.
+    CN_VEC vec = (CN_VEC) malloc(sizeof(struct cn_vec));
+    vec->size      = 0;
+    vec->data      = NULL; //The void of space as we know it.
+    vec->elem_size = size; //Size of each element.
+
+    return vec;
+}
+
+//Add
+void cn_vec_push_back(CN_VEC vec, void* ptr) {
+    if (vec->size == 0) {
+        //Our vector is empty. Initialize the memory properly.
+        vec->data = (void *) malloc(vec->elem_size);
+        if (vec->data != NULL)
+            memcpy(vec->data, ptr, vec->elem_size);
+    } else {
+        //It's already allocated. Let's expand on it.
+        void* new_vec = realloc(vec->data, vec->elem_size * (vec->size + 1));
+        if (!new_vec) {
+            return; //Memory Error
+        } else {
+            vec->data = new_vec;
+        }
+        memcpy(vec->data + (vec->size * vec->elem_size), ptr, vec->elem_size);
+    }
+    vec->size++;
+}
+
+void cn_vec_insert(CN_VEC vec, cnv_uint pos, void* ptr) {
+    if (pos == vec->size - 1)
+        cn_vec_push_back(vec, ptr); //Cheat
+    else
+    if (pos >= vec->size)
+        return; //Out of bounds
+    else {
+        cn_vec_resize(vec, vec->size + 1);
+        //Abuse the fact that memmove uses a buffer to allow overlaying
+        memmove(vec->data + ((pos + 1) * vec->elem_size), vec->data + (pos * vec->elem_size), (vec->size - 1 - pos) * vec->elem_size);
+        memcpy(vec->data + (pos * vec->elem_size), ptr, vec->elem_size);
+    }
+}
+
+//Set
+void cn_vec_resize(CN_VEC vec, cnv_uint size) {
+    if (vec->data == NULL)
+        vec->data = (void *) malloc(vec->elem_size * size);
+    else {
+        if (size == 0)
+            free(vec->data);
+        else {
+            void* new_vec = (void *) realloc(vec->data, vec->elem_size * size);
+            if (!new_vec)
+                return; //Memory Error
+            else
+                vec->data = new_vec;
+        }
+    }
+    vec->size = size;
+}
+
+void cn_vec_set(CN_VEC vec, cnv_uint pos, void* ptr) {
+    if (pos >= vec->size)
+        return; //You failed.
+    memcpy(vec->data + (pos * vec->elem_size), ptr, vec->elem_size);
+}
+
+void cn_vec_delete(CN_VEC vec, cnv_uint pos) {
+    memmove(vec->data +  (pos      * vec->elem_size),
+            vec->data + ((pos + 1) * vec->elem_size),
+            (vec->size - 1 - pos) * vec->elem_size);
+    /*cnv_uint i = pos;
+    for (; i < vec->size - 1; i++)
+        memcpy(vec->data + (i * vec->elem_size), vec->data + ((i + 1) * vec->elem_size), vec->elem_size);*/
+    cn_vec_resize(vec, vec->size - 1);
+}
+
+void cn_vec_copy(CN_VEC dest, CN_VEC src) {
+    if (dest == NULL || src == NULL)
+        return;
+    dest->size      = src->size;
+    dest->elem_size = src->elem_size;
+
+    cn_vec_resize(dest, src->size);
+    memcpy(dest->data, src->data, src->size * src->elem_size);
+}
+
+//Modify
+void cn_vec_clear(CN_VEC vec) {
+    free(vec->data);
+    vec->size = 0;
+}
+
+void cn_vec_swap(CN_VEC vec, cnv_uint pos1, cnv_uint pos2) {
+    if (pos1 != pos2 && pos1 < vec->size && pos2 < vec->size) {
+        cnv_uint i = 0;
+        cnv_byte *b1, *b2;
+
+        //XOR Swap > Malloc & Free... by 0.1 seconds :)
+        b1 = vec->data + (pos1 * vec->elem_size);
+        b2 = vec->data + (pos2 * vec->elem_size);
+
+        for (; i < vec->elem_size; i++) {
+            *b1   ^= *b2;
+            *b2   ^= *b1;
+            *b1++ ^= *b2++;
+        }
+    }
+
+    /*void* tmp = (void *) malloc(vec->elem_size);
+    memcpy(tmp, vec->data + (pos1 * vec->elem_size), vec->elem_size);
+    memcpy(vec->data + (pos1 * vec->elem_size), vec->data + (pos2 * vec->elem_size), vec->elem_size);
+    memcpy(vec->data + (pos2 * vec->elem_size), tmp, vec->elem_size);
+    free(tmp);*/
+}
+
+void cn_vec_reverse(CN_VEC vec) {
+    cnv_uint i  = 0,
+             sr = cn_vec_size(vec),
+             s  = sr / 2; //Round down
+    sr--;
+    for (; i < s; i++) {
+        cn_vec_swap(vec, i, sr - i);
+    }
+}
+
+/*void cn_vec_sort(CN_VEC vec, void (*func)(void*, void*)) {
+    //TODO: Add Sort Functionality (Merge Sort? QSort?)
+}*/
+
+//Get
+void* cn_vec_at(CN_VEC vec, cnv_uint pos) {
+    //Returns the address of an entry in the vector at position "pos".
+    if (pos < 0 || pos >= vec->size)
+        return (void*)0xDEADBEEF; //Nice try. Segfault land for you buddy.
+
+    return vec->data + (pos * vec->elem_size); //I'm in a good mood today.
+}
+
+cnv_uint cn_vec_size(CN_VEC vec) {
+    return vec->size;
+}
+
+cnv_uint cn_vec_element_size(CN_VEC vec) {
+    return vec->elem_size;
+}
+
+cnv_byte cn_vec_empty(CN_VEC vec) {
+    return (vec->size == 0);
+}
+
+void* cn_vec_data(CN_VEC vec) {
+    return vec->data;
+}
+
+//Iteration
+void* cn_vec_begin (CN_VEC vec) { return vec->data; }
+void* cn_vec_end   (CN_VEC vec) { return vec->data + (vec->size * vec->elem_size); }
+void* cn_vec_rbegin(CN_VEC vec) { return vec->data + (vec->size * vec->elem_size) - vec->elem_size; }
+void* cn_vec_rend  (CN_VEC vec) { return vec->data - vec->elem_size; }
+
+//Deinitializer
+void cn_vec_free(CN_VEC vec) {
+    if (vec->size != 0)
+        free(vec->data);
+    free(vec);
+}
---
+++ cn_vec.h
@@ -0,0 +1,116 @@
+/*
+ * CN_Vec Library
+ *
+ * Version 1.0.1 (Last Updated 2016-05-16)
+ *
+ * Description:
+ *     C lacks vectors from C++.
+ *     A CN_Vec is like a vector in C++, only you have to do some additional things for it to work.
+ *     If anything, this library will probably make you switch to C++ for your stuff. :P
+ *
+ *     Changelog of library is located at the bottom of this file.
+ *
+ * Author:
+ *     Clara Ann Nguyen
+ *
+ * For documentation and details on every function in this library, visit the following URL:
+ * http://web.eecs.utk.edu/~ssmit285/lib/cn_vec/index.html
+ */
+
+#ifndef __CN_VEC__
+#define __CN_VEC__
+
+#include <stdlib.h>
+#include <string.h>
+
+//Custom Types
+typedef unsigned int  cnv_uint;
+typedef unsigned char cnv_byte;
+
+//Structs
+typedef struct cn_vec {
+    void*    data;
+    cnv_uint elem_size,
+             size;
+} *CN_VEC;
+
+typedef CN_VEC VECTOR; //For you C++ people...
+
+//Functions
+//Initializer
+CN_VEC   new_cn_vec(cnv_uint);
+
+//Add
+void     cn_vec_push_back   (CN_VEC, void*);
+void     cn_vec_insert      (CN_VEC, cnv_uint, void*);
+
+//Set
+void     cn_vec_resize      (CN_VEC, cnv_uint);
+void     cn_vec_set         (CN_VEC, cnv_uint, void*);
+void     cn_vec_delete      (CN_VEC, cnv_uint);
+void     cn_vec_copy        (CN_VEC, CN_VEC  );
+
+//Modify
+void     cn_vec_clear       (CN_VEC);
+void     cn_vec_swap        (CN_VEC, cnv_uint, cnv_uint);
+void     cn_vec_reverse     (CN_VEC);
+//void     cn_vec_sort        (CN_VEC, void (*)(void*, void*));
+
+//Get
+void*    cn_vec_at          (CN_VEC, cnv_uint);
+cnv_uint cn_vec_size        (CN_VEC);
+cnv_uint cn_vec_element_size(CN_VEC);
+cnv_byte cn_vec_empty       (CN_VEC);
+void*    cn_vec_data        (CN_VEC);
+
+//Iteration
+void*    cn_vec_begin       (CN_VEC);
+void*    cn_vec_end         (CN_VEC);
+void*    cn_vec_rbegin      (CN_VEC);
+void*    cn_vec_rend        (CN_VEC);
+
+//Deinitializer
+void     cn_vec_free        (CN_VEC);
+
+//Macros
+#define cn_vec_deref(ptr, type) \
+    (*(type*)ptr) //lol cheat
+
+#define cn_vec_get(CN_VEC, type, pos) \
+    cn_vec_deref(cn_vec_at(CN_VEC, pos), type) //CHEAT EVEN MORE
+
+#define cn_vec_init(type) \
+    new_cn_vec(sizeof(type)) //For those who hate typing sizeof(...)
+
+#define cn_vec_array(CN_VEC, type) \
+    (type*) cn_vec_data(CN_VEC)
+
+#define cn_vec_traverse(CN_VEC, iterator) \
+    for (iterator = cn_vec_begin(CN_VEC); iterator != cn_vec_end(CN_VEC); iterator++)
+
+#define cn_vec_rtraverse(CN_VEC, iterator) \
+    for (iterator = cn_vec_rbegin(CN_VEC); iterator != cn_vec_rend(CN_VEC); iterator--)
+
+#define cn_vec_front(CN_VEC, type) \
+    cn_vec_get(CN_VEC, type, 0)
+
+#define cn_vec_back(CN_VEC, type) \
+    cn_vec_get(CN_VEC, type, cn_vec_size(CN_VEC) - 1)
+
+#endif
+
+/***************************************\
+    * ** ***    CHANGELOG    *** ** *
+\***************************************/
+
+/*
+    2016-05-16 (1.0.1)
+      - Nope, not the only release.
+      - Reorganized functions.
+      - Added functions "cn_vec_swap", "cn_vec_reverse", "cn_vec_data", "cn_vec_insert", "cn_vec_begin", "cn_vec_end", "cn_vec_rbegin", and "cn_vec_rend".
+      - Added macros "cn_vec_array", "cn_vec_traverse", "cn_vec_rtraverse". "cn_vec_front", and "cn_vec_back".
+      - Swapped the arguments in macro "cn_vec_deref".
+
+    2016-05-16 (1.0.0)
+      - Started writing CN_Vec. Initial (And probably the only) Release.
+*/
